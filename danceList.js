13479f60effc5ca796be382d0100e0df6434a9f6
Raymond Phu <raymond.phu@intel.com>
2024-12-27 21:45:24 -0800

    Add local json db

--- danceList.js
+++ danceList.js
@@ -41,6 +41,39 @@ function getData() {
     });
 };
 
+
+function getLocalData() {
+    fetch('./static/line-dance-db.json')
+        .then(res => res.json())
+        .then(danceList => {
+            for (let danceId in danceList) {
+                let danceObj = danceList[danceId];
+                let dataRow = [];
+                let dance = document.createElement('a');
+                dance.href = danceObj['Stepsheet Link'];
+                dance.target = "_blank";
+                dance.rel = "noopener noreferrer";
+                dance.innerHTML = danceObj.Dance;
+                dataRow.push(dance);
+                dataRow.push(danceObj.Choreographer);
+                dataRow.push(danceObj.Music);
+                dataRow.push(danceObj.Level);
+                dataRow.push(danceObj.Memory.toString());
+                let dance_date = "";
+                if (danceObj["Month Learned"] || danceObj["Year Learned"]) {
+                    dance_date = danceObj["Month Learned"].concat(" ", danceObj["Year Learned"]);
+                }
+
+                dataRow.push(dance_date);
+
+                dataSet.push(dataRow);
+                console.log(typeof(danceObj.Level));
+                console.log(typeof(danceObj["Month Learned"].concat(" ", danceObj["Year Learned"])));
+            };
+            createDataTable(dataSet);
+        })
+};
+
 function createDataTable(dataSet) {
     DataTable.datetime("MMMM YYYY");
     let dt = new DataTable('#danceList', {
@@ -54,11 +87,15 @@ function createDataTable(dataSet) {
         ],
         data: dataSet,
         paging: false,
-        order: [[5, 'desc'], [0, 'asc']]
+        order: [[5, 'desc']],
+        orderFixed: {
+            post: [0, 'asc'],
+        },
     });
 
 };
 
 
 
-getData();
+// getData();
+getLocalData();
